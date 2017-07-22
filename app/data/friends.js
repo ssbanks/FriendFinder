aa2d0acc97baa2cf738a0f0c8939045e47c36547
Shirlon Banks <sssbanks@gmail.com>
2017-07-22 08:40:41 -0700

    update

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = [ {
+	"name" : "Bert",
+	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/t31.0-8/13116244_10107568960519320_7024834722285826624_o.jpg",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+	"name" : "Elmo",
+	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/13315379_10107649806588150_314898033587027263_n.jpg?oh=54f8e3f52844994423b3fec65e9327fb&oe=57E96499",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Big Bird",
+	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/13450242_1233540543375869_9163241940047961665_n.jpg?oh=bb6cd85e9090d902fa663ff864f440e5&oe=5821AB84",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+}, {
+	"name" : "The Grouch",
+	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/13450242_1233540543375869_9163241940047961665_n.jpg?oh=bb6cd85e9090d902fa663ff864f440e5&oe=5821AB84",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+}, {
+	"name" : "Ernie",
+	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/13450242_1233540543375869_9163241940047961665_n.jpg?oh=bb6cd85e9090d902fa663ff864f440e5&oe=5821AB84",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+}];
+
+
+module.exports = friends;
