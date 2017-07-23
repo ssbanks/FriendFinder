ac3325bf93178f0e016c80863d48cdd6be8586d6
Shirlon Banks <sssbanks@gmail.com>
2017-07-22 17:14:57 -0700

    images fix

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,23 @@
 var friends = [ {
 	"name" : "Bert",
-	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/t31.0-8/13116244_10107568960519320_7024834722285826624_o.jpg",
+	"photo" : "../public/images/bert.png",
 	"scores" : [5,5,2,3,1,5,2,1,3,5]
 }, {
 	"name" : "Elmo",
-	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/13315379_10107649806588150_314898033587027263_n.jpg?oh=54f8e3f52844994423b3fec65e9327fb&oe=57E96499",
+	"photo" : "../public/images/elmo.png",
 	"scores" : [1,5,1,1,1,1,1,1,5,5]
 }, {
 	"name" : "Big Bird",
-	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/13450242_1233540543375869_9163241940047961665_n.jpg?oh=bb6cd85e9090d902fa663ff864f440e5&oe=5821AB84",
-	"scores" : [5,3,2,3,2,5,1,3,3,4]
+	"photo" : "../public/images/bigBird.png",
+	"scores" : [2,3,2,3,2,2,1,3,3,3]
 }, {
 	"name" : "The Grouch",
-	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/13450242_1233540543375869_9163241940047961665_n.jpg?oh=bb6cd85e9090d902fa663ff864f440e5&oe=5821AB84",
-	"scores" : [5,3,2,3,2,5,1,3,3,4]
+	"photo" : "../public/images/grouch.png",
+	"scores" : [1,3,2,3,2,1,1,3,3,4]
 }, {
 	"name" : "Ernie",
-	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/13450242_1233540543375869_9163241940047961665_n.jpg?oh=bb6cd85e9090d902fa663ff864f440e5&oe=5821AB84",
-	"scores" : [5,3,2,3,2,5,1,3,3,4]
+	"photo" : "../public/images/ernie.png",
+	"scores" : [3,3,2,3,2,5,5,3,3,4]
 }];
 
 
